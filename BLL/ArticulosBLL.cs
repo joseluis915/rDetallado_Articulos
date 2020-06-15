b98d9f0c1315723fce3aea325ec64865e671abd3
Jose Luis Burgos Hernandez <jose_burgos3@ucne.edu.do>
2020-06-15 19:37:43 -0400

    - Se programo ArticulosBLL (Guardar, Insertar, Modificar, Eliminar, Buscar, GetList, Existe y Get)

--- BLL/ArticulosBLL.cs
+++ BLL/ArticulosBLL.cs
@@ -12,6 +12,153 @@ namespace PrimerParcial_JoseLuis.BLL
 {
     public class ArticulosBLL
     {
-            
+        //=====================================================[ GUARDAR ]=====================================================
+        public static bool Guardar(Articulos articulos)
+        {
+            if (!Existe(articulos.IdArticulo))
+                return Insertar(articulos);
+            else
+                return Modificar(articulos);
+        }
+        //=====================================================[ INSERTAR ]=====================================================
+        private static bool Insertar(Articulos articulos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Articulos.Add(articulos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        //=====================================================[ MODIFICAR ]=====================================================
+        public static bool Modificar(Articulos articulos)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Entry(articulos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        //=====================================================[ ELIMINAR ]=====================================================
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var articulos = contexto.Articulos.Find(id);
+                if (articulos != null)
+                {
+                    contexto.Articulos.Remove(articulos);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        //=====================================================[ BUSCAR ]=====================================================
+        public static Articulos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Articulos articulos;
+            try
+            {
+                articulos = contexto.Articulos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return articulos;
+        }
+        //=====================================================[ GET LIST ]===================================================== 
+        public static List<Articulos> GetList(Expression<Func<Articulos, bool>> criterio)
+        {
+            List<Articulos> lista = new List<Articulos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Articulos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        //=====================================================[ EXISTE ]===================================================== 
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Articulos.Any(d => d.IdArticulo == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+        //=====================================================[ GET ]=====================================================
+        public static List<Articulos> GetArticulos()
+        {
+            List<Articulos> lista = new List<Articulos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Articulos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
     }
 }
---
+++ Migrations/20200615230858_Migracion_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimerParcial_JoseLuis.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    IdArticulo = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Existencia = table.Column<int>(nullable: false),
+                    Costo = table.Column<int>(nullable: false),
+                    ValorInventario = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.IdArticulo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
